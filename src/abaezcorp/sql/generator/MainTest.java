e9f25d1b7e925359993d6b9f56bb1fc354c2098c
= <=>
2018-01-16 08:47:41 +0100

    added examples

--- src/abaezcorp/sql/generator/MainTest.java
+++ src/abaezcorp/sql/generator/MainTest.java
@@ -6,9 +6,11 @@ public class MainTest {
 		SqlGenerator sql = new SqlGenerator();
 		String data;
 
-			//data = sql.table("Books").update("name", "age", "sex").to("abbas", "20", "male").where("username", "abase96").get();
-			//data = sql.table("Books").insert("name", "age").values("abbas", "20", "male").get();
-			data = sql.table("Books").delete().where("abbas", "male").get();
+			//data = sql.table("users").update("name", "age", "sex").to("abbas", "20", "male").where("username", "abase96").get();
+			//data = sql.table("users").insert("name", "age").values("abbas", "20", "male").get();
+			//data = sql.table("users").select().where("username", "abcd").get();
+			//data = sql.table("users").select("first_name", "last_name").where("username", "abcd").get();
+			data = sql.table("users").delete().where("abbas", "male").get();
 			System.out.println(data);
 
 
